36d55970bdc2548f81df471a7afa6b3f86f76970
ps <pio.szo@gmial.com>
2024-01-14 18:56:49 +0100

    Code cleanup

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route('/me', methods=['GET'])
 def me():
-    print("We received GET")
     return render_template("me.html")
 
 @app.route('/contact', methods=['GET', 'POST'])
@@ -12,5 +11,4 @@ def contact():
     if request.method == 'POST':
         print(request.form['message'])
     return render_template("contact.html")
-
 
